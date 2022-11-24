43c811ab1e886e1cd57df8267c84db135faa292f
Godfrey Golova <godfrey.golova@outlook.com>
2022-11-24 14:30:15 +0300

    Add files via upload

---
+++ src/com/codewithadiga/Arrays.java
@@ -0,0 +1,16 @@
+package com.codewithadiga;
+
+public class Arrays
+{
+    public static void main(String[] args)
+    {
+        int[][] numbers = new int[2][3];
+        numbers[0][0] = 1;
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    private static int[][] toString(int[][] numbers)
+    {
+        return numbers;
+    }
+}
