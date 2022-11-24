4570131db44ae6e3a5e474ad273de81a5576efa4
GodfreyAdiga <GodfreyAdiga@KEN-5CG0096HG5>
2022-11-24 14:51:34 +0300

    Type Casting

---
+++ src/com/codewithadiga/MathClass.java
@@ -0,0 +1,10 @@
+package com.codewithadiga;
+
+public class MathClass
+{
+    public static void main()
+    {
+        int result = (int) Math.round(Math.random()*100);
+        System.out.println(result);
+    }
+}
