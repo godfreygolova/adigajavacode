7d79f98b8e75bdd9ac487ed6fe99f475f1939f51
GodfreyAdiga <GodfreyAdiga@KEN-5CG0096HG5>
2022-11-24 14:35:05 +0300

    Type Casting

---
+++ src/com/codewithadiga/CastTypeCasting.java
@@ -0,0 +1,12 @@
+package com.codewithadiga;
+
+public class CastTypeCasting
+{
+    public static void main(String[] args)
+    {
+        //implicit casting
+        short x = 1;
+        int y= x+2;
+        System.out.println(y);
+    }
+}
